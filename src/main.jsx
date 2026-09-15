9dffaf5c687d76b8c3437168ad8bb0a1b74fea23
Maya Valerio <mayaivalerio@icloud.com>
2026-09-14 22:16:47 -0400

    troubleshooting w/ kevins idea

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,9 @@ const router = createBrowserRouter([
   {path: "/", element: <App/>},
   {path: "/about", element: <About/>},
   {path: "/projects", element: <Projects/>}
-]);
+], {
+  basename: "/mayavalerio-v2",
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
